7131101bf8adf12bb1c515277c8a5545a2c5dad0
maaz619 <maazahmedlfc@gmail.com>
2023-02-15 22:07:00 +0530

    added loader with fixed color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import React from 'react'
 import './App.css'
+import Loading from './components/Loader/Loader'
+import Home from './pages/Home'
 
 function App() {
-  return <div className="App">Entry point</div>
+  const [initialLoad, setLoad] = React.useState(true)
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoad(false)
+    }, 3500)
+  }, [])
+  return <div className="App">{initialLoad ? <Loading /> : <Home />}</div>
 }
 
 export default App
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,20 @@
+import { loader } from '../../assets/images/index'
+
+function Loading() {
+  return (
+    <div
+      className="bg-white flex items-center flex-col justify-center h-full w-full fixed top-0 ;
+            z-index: 200000;"
+    >
+      <img src={loader} alt="gdglogo" className="rounded-full w-48 h-48 " />
+      <div className="flex items-center justify-center h-40">
+        <div className=" bg-google-blue h-8 w-8 rounded-full mx-4 animate-bounce animation-delay-0 " />
+        <div className=" bg-google-red h-8 w-8 rounded-full mx-4 animate-bounce animation-delay-75" />
+        <div className=" bg-google-yellow h-8 w-8 rounded-full mx-4 animate-bounce animation-delay-150" />
+        <div className=" bg-google-green h-8 w-8 rounded-full mx-4 animate-bounce animation-delay-225" />
+      </div>
+    </div>
+  )
+}
+
+export default Loading
