59d66549d11d79eb1e30da1f96ede2de02ec4c66
maaz619 <maazahmedlfc@gmail.com>
2023-02-19 04:17:15 +0530

    feat: back to top button

---
+++ src/components/GoTop/GoTop.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BsArrowUpSquareFill } from 'react-icons/bs';
+
+const GoTop: React.FC<any> = (props) => {
+  return (
+    <div>
+      <>
+        <div className={`${props.showGoTop}`} onClick={props.scrollUp}>
+          <button className=" block">
+            <div className=" block fixed w-10 h-10 rounded-3xl right-4 bottom-4  cursor-pointer leading-7 text-center ">
+              <BsArrowUpSquareFill size={30} />
+            </div>
+          </button>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default GoTop;
