fcb4927b9feb45e10b08627d0ba0b28f0411065d
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2023-02-27 16:25:12 +0530

    Made homeSocials responsive

--- src/components/HomeDisplay/HomeSocials.tsx
+++ src/components/HomeDisplay/HomeSocials.tsx
@@ -20,55 +20,88 @@ const HomeSocials = () => {
 
   const [disabledSocial, setdisabledSocial] = useState(['']);
   useEffect(() => {
-    getFeature().then(data => {
+    getFeature().then((data) => {
       if (data) setdisabledSocial(data.disabledSocial);
     });
   }, []);
 
   return (
     <div
-      className={`${CurrentTheme() === 'white'
-        ? 'bg-white text-black'
-        : 'bg-black text-white'
-        }`}
+      className={`${
+        CurrentTheme() === 'white'
+          ? 'bg-white text-black'
+          : 'bg-black text-white'
+      }`}
     >
-      <div className="flex flex-col items-center justify-center w-full mt-10 mb-5 pt-5">
+      <div className="flex flex-col items-center justify-center w-full mt-10 mb-2 lg:mb-5 pt-5">
         <div className="flex flex-row ">
-          <div className={`event-sec-2 block uppercase ${CurrentTheme() === 'white' ? 'strokeme text-white ' : ' strokeme-w text-black'}`}>
+          <div
+            className={`font-medium leading-14 text-[20px] md:text-[54px] block uppercase ${
+              CurrentTheme() === 'white'
+                ? 'strokeme text-white '
+                : ' strokeme-w text-black'
+            }`}
+          >
             Follow us for&nbsp;
           </div>
           <p
-            className={`event-sec-2  no-shadow uppercase
+            className={`font-medium leading-14 text-[20px] md:text-[54px] no-shadow uppercase
               }`}
           >
             UPDATES
           </p>
         </div>
       </div>
-      <div className="flex flex-row items-center justify-center space-x-7 pb-5">
-        {
-          socialContent?.social?.map((el, i) => {
-            return (
-              (disabledSocial?.every(i => i !== el.imgSrc)) ? (
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={el.hyperlink}
-                  key={i}
-                >
-                  <div className="relative inline-block text-left group ">
-                    {(el.imgSrc === 'facebook') ? <FacebookSVGIcon fill="currentColor" className="w-12 h-12" /> : null}
-                    {(el.imgSrc === 'twitter') ? <TwitterSVGIcon fill="currentColor" className="w-12 h-12" /> : null}
-                    {(el.imgSrc === 'instagram') ? <InstagramSVGIcon fill="currentColor" className="w-12 h-12" /> : null}
-                    {(el.imgSrc === 'linkedin') ? <LinkedInSVGIcon fill="currentColor" className="w-12 h-12" /> : null}
-                    {(el.imgSrc === 'github') ? <GitHubSVGIcon fill="currentColor" className="w-12 h-12" /> : null}
-                    {(el.imgSrc === 'gmail') ? <GmailSVGIcon fill="currentColor" className="w-12 h-12" /> : null}
-                  </div>
-                </a>
-              ) : null
-            )
-          })
-        }
+      <div className="flex flex-row items-center justify-center lg:space-x-7 space-x-2 pb-5">
+        {socialContent?.social?.map((el, i) => {
+          return disabledSocial?.every((i) => i !== el.imgSrc) ? (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={el.hyperlink}
+              key={i}
+            >
+              <div className="relative inline-block text-left group ">
+                {el.imgSrc === 'facebook' ? (
+                  <FacebookSVGIcon
+                    fill="currentColor"
+                    className="w-8 h-8 lg:w-12 lg:h-12"
+                  />
+                ) : null}
+                {el.imgSrc === 'twitter' ? (
+                  <TwitterSVGIcon
+                    fill="currentColor"
+                    className="w-8 h-8 lg:w-12 lg:h-12"
+                  />
+                ) : null}
+                {el.imgSrc === 'instagram' ? (
+                  <InstagramSVGIcon
+                    fill="currentColor"
+                    className="w-8 h-8 lg:w-12 lg:h-12"
+                  />
+                ) : null}
+                {el.imgSrc === 'linkedin' ? (
+                  <LinkedInSVGIcon
+                    fill="currentColor"
+                    className="w-8 h-8 lg:w-12 lg:h-12"
+                  />
+                ) : null}
+                {el.imgSrc === 'github' ? (
+                  <GitHubSVGIcon
+                    fill="currentColor"
+                    className="w-8 h-8 lg:w-12 lg:h-12"
+                  />
+                ) : null}
+                {el.imgSrc === 'gmail' ? (
+                  <GmailSVGIcon
+                    fill="currentColor"
+                    className="w-8 h-8 lg:w-12 lg:h-12"
+                  />
+                ) : null}
+              </div>
+            </a>
+          ) : null;
+        })}
       </div>
     </div>
   );
