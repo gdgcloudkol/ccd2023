f715b9dfb7647a090d8e3d04b7974432b5c01c0e
Srijan Gupta <gupta.srijan94@gmail.com>
2023-02-28 13:45:28 +0530

    feat: 404 page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,12 @@
-import {
-  useContext,
-  useEffect,
-  useState,
-  useLayoutEffect,
-  ReactElement
-} from 'react';
-import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import NavbarPage from './components/Navbar/Navbar';
 import Coc from './pages/Coc';
 import Dashboard from './pages/Dashboard';
 import FaqPage from './pages/Faq';
 import Home from './pages/Home';
 import ApiLogin from './pages/Login';
+import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import Schedule from './pages/Schedule/Schedule';
 import Signup from './pages/Signup';
@@ -19,14 +14,6 @@ import Speakers from './pages/Speakers';
 import Tickets from './pages/Tickets';
 import { getFeature } from './services/feature.service';
 import { LoggedInContext } from './services/state.service';
-
-const ScrollToTop = ({ children }: { children: ReactElement }) => {
-  const location = useLocation();
-  useLayoutEffect(() => {
-    document.documentElement.scrollTo(0, 0);
-  }, [location.pathname]);
-  return children;
-};
 
 const Router = () => {
   const { loggedInState } = useContext(LoggedInContext);
@@ -42,40 +29,38 @@ const Router = () => {
       <nav className="sticky top-0 z-30 w-full bg-white dark:bg-black dark:bg-opacity-0 backdrop-filter backdrop-blur-md bg-opacity-30 ">
         <NavbarPage />
       </nav>
-      <ScrollToTop>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          {navRule?.every((item) => '/schedule' !== item) ? (
-            <Route path="/schedule" element={<Schedule />} />
-          ) : null}
-          {navRule?.every((item) => '/speakers' !== item) ? (
-            <Route path="/speakers" element={<Speakers />} />
-          ) : null}
-          {navRule?.every((item) => '/faq' !== item) ? (
-            <Route path="/faq" element={<FaqPage />} />
-          ) : null}
-          {navRule?.every((item) => '/tickets' !== item) ? (
-            <Route path="/tickets" element={<Tickets />} />
-          ) : null}
-          {navRule?.every((item) => '/codeofconduct' !== item) ? (
-            <Route path="/code-of-conduct" element={<Coc />} />
-          ) : null}
-          {navRule?.every((item) => '/signup' !== item) ? (
-            <Route path="/signup" element={<Signup />} />
-          ) : null}
-          {navRule?.every((item) => '/login' !== item) ? (
-            <Route path="/login" element={<ApiLogin />} />
-          ) : null}
-          {navRule?.every((item) => '/profile' !== item) && loggedInState ? (
-            <Route path="/profile" element={<Profile />} />
-          ) : null}
-          {navRule?.every((item) => '/dashboard' !== item) && loggedInState ? (
-            <Route path="/dashboard" element={<Dashboard />} />
-          ) : null}
-          <Route path="/*" element={<Home />} />
-        </Routes>
-      </ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        {navRule?.every((item) => '/schedule' !== item) ? (
+          <Route path="/schedule" element={<Schedule />} />
+        ) : null}
+        {navRule?.every((item) => '/speakers' !== item) ? (
+          <Route path="/speakers" element={<Speakers />} />
+        ) : null}
+        {navRule?.every((item) => '/faq' !== item) ? (
+          <Route path="/faq" element={<FaqPage />} />
+        ) : null}
+        {navRule?.every((item) => '/tickets' !== item) ? (
+          <Route path="/tickets" element={<Tickets />} />
+        ) : null}
+        {navRule?.every((item) => '/codeofconduct' !== item) ? (
+          <Route path="/code-of-conduct" element={<Coc />} />
+        ) : null}
+        {navRule?.every((item) => '/signup' !== item) ? (
+          <Route path="/signup" element={<Signup />} />
+        ) : null}
+        {navRule?.every((item) => '/login' !== item) ? (
+          <Route path="/login" element={<ApiLogin />} />
+        ) : null}
+        {navRule?.every((item) => '/profile' !== item) && loggedInState ? (
+          <Route path="/profile" element={<Profile />} />
+        ) : null}
+        {navRule?.every((item) => '/dashboard' !== item) && loggedInState ? (
+          <Route path="/dashboard" element={<Dashboard />} />
+        ) : null}
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </HashRouter>
   );
 };
