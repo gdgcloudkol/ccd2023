7fe0ebee580a8df9b107082f88b5c3a2f5f72232
Srijan Gupta <gupta.srijan94@gmail.com>
2023-03-06 00:39:57 +0530

    fix: loader

--- src/pages/Speakers.tsx
+++ src/pages/Speakers.tsx
@@ -38,21 +38,21 @@ const Speakers = () => {
 
   return (
     <>
-      {!speakersDetails.length && (
-        <div className="flex justify-center items-center h-screen bg-transparent">
-          <GoogleDotsLoader />
-        </div>
+      {!speakersDetails.length ? (
+        <GoogleDotsLoader />
+      ) : (
+        <>
+          <div className="flex justify-center items-center flex-col px-5 pt-5">
+            <div className="text-2xl lg:text-3xl font-normal text-g-gray-8 dark:text-white">
+              {speakerContent?.title}
+            </div>
+            <div className="text-base max-w-2xl w-fit text-center font-light text-g-gray-5 dark:text-white mt-2">
+              {speakerContent?.description}
+            </div>
+          </div>
+          <PeopleGrid peopleGrid={speakersDetails} rule={speakerRule} />
+        </>
       )}
-
-      <div className="flex justify-center items-center flex-col px-5 pt-5">
-        <div className="text-2xl lg:text-3xl font-normal text-g-gray-8 dark:text-white">
-          {speakerContent?.title}
-        </div>
-        <div className="text-base max-w-2xl w-fit text-center font-light text-g-gray-5 dark:text-white mt-2">
-          {speakerContent?.description}
-        </div>
-      </div>
-      <PeopleGrid peopleGrid={speakersDetails} rule={speakerRule} />
     </>
   );
 };
