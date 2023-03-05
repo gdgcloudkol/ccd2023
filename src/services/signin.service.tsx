5ab46ecd0f8082ad86257010ca2b4be2f6020f87
maaz619 <maazahmedlfc@gmail.com>
2023-03-06 02:20:06 +0530

    feat: verify email via frontend and resend email option added

--- src/services/signin.service.tsx
+++ src/services/signin.service.tsx
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { LoginData, SignInPayload, SignUpPayload } from '../assets/models/login/datatype';
-import { ACCESS_TOKEN_KEY, BASE_LOGIN_URI, BASE_REGISTRATION_URI, HOME_ROUTE, LOGGED_IN_KEY } from './constants';
+import { ACCESS_TOKEN_KEY, BASE_EMAIL_RESEND_URL, BASE_EMAIL_VERIFICATION_URL, BASE_LOGIN_URI, BASE_LOGOUT_URI, BASE_REGISTRATION_URI, HOME_ROUTE, LOGGED_IN_KEY } from './constants';
+import { NavigateFunction } from 'react-router-dom';
 
 export async function ApiSignIn(
   payload: SignInPayload,
@@ -26,10 +27,8 @@ export async function ApiSignup(
 ): Promise<AxiosResponse> {
   try {
     const res = await axios.post(BASE_REGISTRATION_URI, payload);
-    if (res.status === 200) {
-      return res
-    } else if (res.status === 400) {
-      return res.data;
+    if (res?.status !== 200) {
+      throw "Signup Error";
     }
     return res;
   } catch (e: any) {
@@ -37,9 +36,49 @@ export async function ApiSignup(
   }
 }
 
-export function ApiLogout(setLoggedInState: any, navigate: any) {
-  // https://api.gdgcloud.kolkata.dev/auth/logout/
-  localStorage.removeItem(LOGGED_IN_KEY);
-  setLoggedInState(false);
-  navigate(HOME_ROUTE);
+export async function ApiLogout(
+  setLoggedInState: React.Dispatch<React.SetStateAction<boolean>>,
+  navigate: NavigateFunction
+) {
+  try {
+    const res = await axios.get(BASE_LOGOUT_URI);
+    if (res?.status !== 200) {
+      throw "Logout Error";
+    }
+    localStorage.removeItem(LOGGED_IN_KEY);
+    setLoggedInState(false);
+    navigate(HOME_ROUTE);
+    return res;
+  } catch (e: any) {
+    return e.response;
+  }
 }
+
+export async function ApiEmailVerification(
+  payload: string
+): Promise<AxiosResponse> {
+  try {
+    let finalUrl = `${BASE_EMAIL_VERIFICATION_URL}${payload}`;
+    const res = await axios.get(finalUrl);
+    if (res?.status !== 200) {
+      throw "Verification Error";
+    }
+    return res;
+  } catch (e: any) {
+    return e.response;
+  }
+};
+
+export async function ApiResendVerification(
+  payload: string
+): Promise<AxiosResponse> {
+  try {
+    const res = await axios.post(BASE_EMAIL_RESEND_URL, { email: payload });
+    if (res?.status !== 200) {
+      throw "Resend Verification Error";
+    }
+    return res;
+  } catch (e: any) {
+    return e.response;
+  }
+};
